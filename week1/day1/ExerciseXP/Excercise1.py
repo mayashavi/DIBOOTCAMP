c6390c616550a780f011b500a0fc3e9bed5262b8
Maya Shavit <mayashavit@MacBook-Pro-2119.local>
2025-11-09 14:39:46 +0200

    Describe what you did here

---
+++ week1/day1/ExerciseXP/Excercise1.py
@@ -0,0 +1,52 @@
+sentence = 'Hello world\n'
+print(sentence * 4)
+
+result = (99 ** 3) * 8
+print(result)
+
+5 < 3 #FALSE
+3 == 3 #TRUE
+3 == "3" #FALSE
+
+"Hello" == "hello" #FALSE
+
+computer_brand = 'MacBook'
+print(f'I have a {computer_brand} computer')
+
+name = 'Maya'
+age = 24
+shoe_size = 38
+
+info = f'My name is {name}, I\’m {age} years old, and my shoe size is {shoe_size}'
+
+print(info)
+
+a = 2
+b = 1
+
+if a > b:
+    print('Hello World')
+
+number = int(input('Enter a number: '))
+
+if number % 2 == 0:
+    print('The number is even')
+else:
+    print('The number is odd')
+
+my_name = 'Maya'
+
+user_name = input("What’s your name? ")
+
+if user_name.strip().capitalize() == my_name:
+    print('Best! Name! EVER!!!!!')
+else:
+    print(f'This is a "Maya" only zone!')
+
+height = int(input('What is your height in centimeters? '))
+
+if height > 145:
+    print('You\’re tall enough to ride!')
+else:
+    print('You are not tall enough to ride')
+
